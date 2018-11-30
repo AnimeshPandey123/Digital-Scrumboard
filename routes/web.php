0cd3546d6b859b537734a2e57e575701abb71e78
Animesh Pandey <ani.mesh13578@gmail.com>
2018-11-30 19:03:45 +0545

    Created a dashboard page

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,16 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin/dashboard', function () {
+	return view('admin.dashboard');
+});
