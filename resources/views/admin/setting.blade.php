49b5db6a6c3c9ba72f702adff6dfbfa5620e6353
Nischal10 <nis.kharel@gmail.com>
2018-12-23 13:27:14 +0545

    project page content + setting page content

--- resources/views/admin/setting.blade.php
+++ resources/views/admin/setting.blade.php
@@ -1,1 +1,20 @@
 @extends('layouts.admin')
+
+@section('content')
+	<h6 class="text-lightgrey">
+		<i class="fas fa-cog"></i>
+		<span>General</span>
+	</h6>
+	<h6 class="text-lightgrey">
+		<i class="fas fa-user-plus"></i>
+		<span>Linked Profile</span>
+	</h6>
+	<h6 class="text-lightgrey">
+		<i class="fas fa-wrench"></i>
+		<span>Customize</span>
+	</h6>
+	<h6 class="text-lightgrey">
+		<i class="fas fa-globe-asia"></i>
+		<span>Notifications</span>
+	</h6>
+@endsection
