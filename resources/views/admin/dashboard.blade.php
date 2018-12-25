ffd1b531e9f43d821bb362ebd46729515ee3d56a
Aakash Raj Dahal <akashrajdahal@gmail.com>
2018-12-25 20:44:46 +0545

    Filled in the Recents Pab

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,7 +10,27 @@
 				<div class="row">
 					<div class="col-md-6">
 						<div>
-							
+							<canvas id="pie-chart" width="100" height="100"></canvas>
+							<!-- Pie Chart -->
+							<script>
+								new Chart(document.getElementById("pie-chart"), {
+								    type: 'pie',
+								    data: {
+								      labels: ["College Project", "Mind Map", "Designer's Club"],
+								      datasets: [{
+								        label: "Tasks",
+								        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
+								        data: [2478,5267,734]
+								      }]
+								    },
+								    options: {
+								      title: {
+								        display: true,
+								        text: 'Tasks Division'
+								      }
+								    }
+								});
+							</script>
 						</div>
 					</div>
 					<div class="col-md-6">
@@ -83,7 +103,93 @@
 				</span><br>
 				<div class="cardz">
 					<div class="dsb_cardz">
-						yolo
+						<i>
+							<span style="float:right;">
+								15th Dec
+							</span>
+						</i>
+						<table class="table table-sm table-borderless">
+							<tr>
+								<td>
+									<img src="{{asset('images/picture.jpg')}}" alt="" class="rec_pic">
+								</td>
+								<td>
+									<b class="text-theme">Melissa Willaims</b><br> completed 2 tasks in <b class="text-theme">College Project</b>
+								</td>
+							</tr>
+						</table>
+					</div>
+					<br>
+					<div class="dsb_cardz">
+						<i>
+							<span style="float:right;">
+								15th Dec
+							</span>
+						</i>
+						<table class="table table-sm table-borderless">
+							<tr>
+								<td>
+									<img src="{{asset('images/picture.jpg')}}" alt="" class="rec_pic">
+								</td>
+								<td>
+									<b class="text-theme">Melissa Willaims</b><br> completed 5 tasks in <b class="text-theme">Mind Map</b>
+								</td>
+							</tr>
+						</table>
+					</div>
+					<br>
+					<div class="dsb_cardz">
+						<i>
+							<span style="float:right;">
+								15th Dec
+							</span>
+						</i>
+						<table class="table table-sm table-borderless">
+							<tr>
+								<td>
+									<img src="{{asset('images/picture.jpg')}}" alt="" class="rec_pic">
+								</td>
+								<td>
+									<b class="text-theme">Melissa Willaims</b><br> created a new project <b class="text-theme">DotA Team</b> and added 17 memebers
+								</td>
+							</tr>
+						</table>
+					</div>
+					<br>
+					<div class="dsb_cardz">
+						<i>
+							<span style="float:right;">
+								15th Dec
+							</span>
+						</i>
+						<table class="table table-sm table-borderless">
+							<tr>
+								<td>
+									<img src="{{asset('images/picture.jpg')}}" alt="" class="rec_pic">
+								</td>
+								<td>
+									<b class="text-theme">Melissa Willaims</b><br> created a new project <b class="text-theme">Reddit Nepal Mod</b> and added 3 memebers
+								</td>
+							</tr>
+						</table>
+					</div>
+					<br>
+					<div class="dsb_cardz">
+						<i>
+							<span style="float:right;">
+								15th Dec
+							</span>
+						</i>
+						<table class="table table-sm table-borderless">
+							<tr>
+								<td>
+									<img src="{{asset('images/picture.jpg')}}" alt="" class="rec_pic">
+								</td>
+								<td>
+									<b class="text-theme">Melissa Willaims</b><br> archived 25 tasks in <b class="text-theme">College Project</b>
+								</td>
+							</tr>
+						</table>
 					</div>
 				</div>
 			</div>
